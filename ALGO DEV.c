6f4b8fea46264bfa116130793a79db4a26589c31
siba-00695 <konatepapa686@gmail.com>
2025-06-18 16:11:46 +0000

    Update ALGO DEV.c

--- ALGO DEV.c
+++ ALGO DEV.c
@@ -1,1 +1,27 @@
-//REMPLIE TAB 
+//definir la structure par ismail
+
+
+//remplir tab par ismail
+
+
+//afficher tab par thierno
+
+//somme tab par thierno
+
+
+//fontion min tab par ousmane
+
+//fontion max tab par ousmane
+
+
+//fonction somme par objet par alhousseynou
+
+
+//procedure facture par etudiant par alhousseynou
+
+
+//methode main par alhousseynou
+
+
+
+
